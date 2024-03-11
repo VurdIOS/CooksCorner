691babac731489c446bfaa32f79b91f5c301cf84
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2024-03-11 20:27:09 +0300

    Initial Commit

---
+++ CooksCorner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CooksCorner
+//
+//  Created by Камаль Атавалиев on 11.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
