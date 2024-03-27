8523461fa105cc614b5be8e9b774aa6c0013cb03
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2024-03-27 20:19:42 +0300

    -fix my fails

--- CooksCorner/Profile/ProfileViewController.swift
+++ CooksCorner/Profile/ProfileViewController.swift
@@ -11,48 +11,17 @@ class ProfileViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+
+    
     override func loadView() {
         view = contentView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        setUpCollectionView()
     }
     
-    private func setUpCollectionView() {
-        contentView.recipeCollectionView.delegate = self
-        contentView.recipeCollectionView.dataSource = self
-    }
-
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-}
-
-extension ProfileViewController:  UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        viewModel.getRecipeCount()
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecipeCollectionViewCell.reuseIdentifier, for: indexPath) as! RecipeCollectionViewCell
-        cell.configure(recipe: viewModel.getRecipe(index: indexPath.row))
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let recipe = viewModel.getRecipe(index: indexPath.row)
-        let detailRecipeViewController = RecipeDetailNewViewController(viewModel: viewModel.getDataForDetailViewModel(recipe: recipe))
-        navigationController?.pushViewController(detailRecipeViewController, animated: true)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 169, height: 209)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
     }
 }
--- CooksCorner/Profile/ProfileViewModel.swift
+++ CooksCorner/Profile/ProfileViewModel.swift
@@ -8,24 +8,9 @@
 import Foundation
 
 protocol ProfileViewModelProtocol: AnyObject {
-    func getRecipeCount() -> Int
-    func getRecipe(index: Int) -> Recipe
-    func getDataForDetailViewModel(recipe: Recipe) -> RecipeDetailNEWViewModelProtocol
 }
 
 class ProfileViewModel: ProfileViewModelProtocol {
+
     
-    private var recipeData: [Recipe] = Recipe.getMokeData()
-    
-    func getRecipeCount() -> Int {
-        recipeData.count
-    }
-    
-    func getRecipe(index: Int) -> Recipe {
-        recipeData[index]
-    }
-    
-    func getDataForDetailViewModel(recipe: Recipe) -> RecipeDetailNEWViewModelProtocol {
-        RecipeDetailNEWViewModel(recipe: recipe)
-    }
 }
